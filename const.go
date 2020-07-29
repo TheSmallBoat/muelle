da8fb4bfd2dda3502c1b6683f1c656be2bdf4673
TheSmallBoat <abe.chua@qq.com>
2020-07-30 11:03:16 +1200

    initial commit

---
+++ const.go
@@ -0,0 +1,21 @@
+package muelle
+
+const (
+	ServiceTwin      = "Muelle-Twin"
+	ServicePublish   = "Muelle-Pub"
+	ServiceSubscribe = "Muelle-Sub"
+
+	ActionHeader    = "HA"
+	TopicHeader     = "HT"
+	QosHeader       = "HQ"
+	BodyTitleHeader = "HBT"
+
+	ActionPublish   = "AP"
+	ActionSubscribe = "AS"
+
+	ResponsePublishHeader   = "RP"
+	ResponseSubscribeHeader = "RS"
+
+	ResponseSuccess = "s"
+	ResponseFailure = "f"
+)
